c0dc12e25f6f3cda63a65d16c3537afa37f0982c
Your Name <you@example.com>
2022-05-30 11:26:49 -0400

    pas le temps

--- srcs/containers/wordpress/requirements/entry.sh
+++ srcs/containers/wordpress/requirements/entry.sh
@@ -1,2 +1,3 @@
+wp-cli config create --dbname=wordpress --dbuser=jbondri --dbpass=MDPJ --dbhost=mariadb:330      6 --dbcharset=utf8 --path=/var/www/wordpress --allow-root --skip-check
+wp-cli core install --url=jbondri.42.fr --title=Jbond --admin_user=jbondri --admin_password=MDPJ --admin_email=jbondri@student.42.fr --path=/var/www/wordpress --allow-root --skip-email;
 /usr/sbin/php-fpm7.3 -R -F;
-#wp-cli core install --url=jbondri.42.fr --title=Jbond --admin_user=jbondri --admin_password=MDPJ --admin_email=jbondri@student.42.fr --path=/var/www/wordpress --allow-root --skip-email;
